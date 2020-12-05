5d18f363ddb3242cf971df0014528cc0b8dd54d4
Izaak Prats <izaak.prats@gmail.com>
2020-12-04 22:47:12 -0700

    add day5. update test.py

--- day3/test.py
+++ day3/test.py
@@ -1,11 +1,5 @@
 import puzzle
 
-
-def main():
-    data = puzzle.parse(file("input.txt").readlines())
-    answer = puzzle.solve(data)
-    print("ANSWER IS: ", answer)
-
-
-if __name__ == '__main__':
-    main()
+data = puzzle.parse(file("input.txt").readlines())
+answer = puzzle.solve(data)
+print("ANSWER IS: ", answer)
---
+++ day5/puzzle.py
@@ -0,0 +1,47 @@
+
+
+def parse(lines):
+    return lines
+
+
+def solve(data):
+
+    seat_ids = []
+
+    for a in data:
+        a.rstrip()
+        rows = a[0:7]
+        cols = a[7:]
+
+        rows_lower = 0
+        rows_upper = 127
+        for row in rows:
+            if row == 'B':
+                rows_lower += (rows_upper - rows_lower) / 2
+                rows_lower += 1
+
+            elif row == 'F':
+                rows_upper -= (rows_upper - rows_lower) / 2
+                rows_upper -= 1
+
+        cols_lower = 0
+        cols_upper = 7
+        for col in cols:
+            if col == 'R':
+                cols_lower += (cols_upper - cols_lower) / 2
+                cols_lower += 1
+
+            elif col == 'L':
+                cols_upper -= (cols_upper - cols_lower) / 2
+                cols_upper -= 1
+
+        seat_id = (rows_lower * 8) + cols_lower
+        seat_ids.append(seat_id)
+
+    # myseat
+    for seat in range(min(seat_ids), max(seat_ids)):
+        if seat not in seat_ids:
+            return seat
+
+    # highest
+    return max(seat_ids)
